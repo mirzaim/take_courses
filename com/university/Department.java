c501c9381665f12b0b6cf88c34a362043bd82691
Morteza <mortezamirzai120@gmail.com>
2019-04-17 18:12:07 +0430

    Complit Department class.

--- com/university/Department.java
+++ com/university/Department.java
@@ -1,10 +1,18 @@
 package com.university;
 
+import java.util.ArrayList;
+
 public class Department {
     private String name;
+    private ArrayList<Student> students;
+    private ArrayList<Course> courses;
+    private ArrayList<Professor> professors;
 
     public Department(String name) {
         this.name = name;
+        students = new ArrayList<>();
+        courses = new ArrayList<>();
+        professors = new ArrayList<>();
     }
 
     public String getName() {
@@ -12,39 +20,46 @@ public class Department {
     }
 
     public void addStudent(Student student) {
-
+        if (student.getDepartment().equals(this))
+            students.add(student);
     }
 
     public Student[] getStudents() {
-        return null;
+        return students.toArray(new Student[0]);
     }
 
     public void removeStudent(Student student) {
-
+        if (student.getDepartment().equals(this))
+            students.remove(student);
+        ;
     }
 
     public void addCourse(Course course) {
-
+        if (course.getDepartment().equals(this))
+            courses.add(course);
     }
 
     public Course[] getCourses() {
-        return null;
+        return courses.toArray(new Course[0]);
     }
 
     public void removeCourse(Course course) {
-
+        if (course.getDepartment().equals(this))
+            courses.remove(course);
     }
 
-    public void addProfessor(Professor student) {
-
+    public void addProfessor(Professor professor) {
+        if (professor.getDepartment().equals(this))
+            professors.add(professor);
     }
 
     public Professor[] getProfessors() {
-        return null;
+        return professors.toArray(new Professor[0]);
     }
 
     public void removeProfessor(Professor professor) {
-
+        if (professor.getDepartment().equals(this))
+            professors.remove(professor);
     }
 
     @Override
